f3ba2a09cd56738ca337f2f7fb13e88a927152ae
Sonia Seuc <sonia.seuc@gmail.com>
2020-07-18 11:22:34 -0500

    add routing components,see components on browser, style login

--- src/app/router–routing.module.ts
+++ src/app/router–routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+//components
 import { LoginComponent } from './components/user/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Page404Component } from './components/page404/page404.component';
+import { RegisterComponent } from './components/user/register/register.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'nav', component: NavbarComponent},
+  {path: 'register', component: RegisterComponent },
+  {path: '**', component: Page404Component },
 ];
 
 @NgModule({
